3964118f6dcc630c719a0de6cc466c03c5c6f6a4
MrActividades854 <kevinjoelescobar2020@gmail.com>
2025-12-05 16:58:45 -0500

    Update face-recognition.js

--- public/js/face-recognition.js
+++ public/js/face-recognition.js
@@ -2,7 +2,7 @@
 // Un solo pipeline TinyFaceDetector para TODO el sistema
 
 import { CONFIG } from "./config.js";
-import * as faceapi from 'https://cdn.jsdelivr.net/npm/face-api.js';
+import * as faceapi from 'https://cdn.jsdelivr.net/npm/@vladmandic/face-api/dist/face-api.esm.js';
 
 export default class FaceRecognitionManager {
     constructor({ modelPath = '/models', getActiveVideo = () => null, onNotification = () => {} } = {}) {
