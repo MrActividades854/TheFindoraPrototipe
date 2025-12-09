a6d6145dc8ad60e8e3282164b62aa4784d09057a
MrActividades854 <kevinescobardavila.est@comfandi.edu.co>
2025-12-09 16:33:17 -0500

    un poco de todo, settings y reutilizables

---
+++ public/findorasections/include.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const elements = document.querySelectorAll("[include-html]");
+
+    elements.forEach(async (el) => {
+        const file = el.getAttribute("include-html");
+
+        try {
+            const resp = await fetch(file);
+
+            if (!resp.ok) {
+                el.innerHTML = "No se pudo cargar el componente.";
+                return;
+            }
+
+            el.innerHTML = await resp.text();
+        } catch (error) {
+            el.innerHTML = "Error cargando el archivo.";
+        }
+    });
+});
