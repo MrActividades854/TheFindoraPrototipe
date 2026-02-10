bb35c7b5b724d72d0cd005b3ab8d7233fa0bfb54
MrActividades854 <kevinescobardavila.est@comfandi.edu.co>
2026-02-10 16:36:54 -0500

    uuu

--- public/js/face-recognition.js
+++ public/js/face-recognition.js
@@ -84,12 +84,6 @@ export default class FaceRecognitionManager {
         console.log("🔄 Iniciando carga de perfiles...");
         const useWS = localStorage.getItem("useWebSocket") === "true";
         console.log("🌐 WebSocket activado:", useWS);
-        
-        if (!useWS) {
-            console.warn("⚠️ WebSocket OFF → Inicializando sin perfiles");
-            this._initEmptyMatcher();
-            return [];
-        }
 
         try {
             const cached = localStorage.getItem(CONFIG.PROFILES_KEY);
