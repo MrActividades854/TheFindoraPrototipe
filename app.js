e60568e12560c1c9905b8c05642a73b477bc5ab6
MrActividades854 <kevinjoelescobar2020@gmail.com>
2026-03-31 09:34:45 -0500

    Update app.js

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-    res.redirect("/findorasections/mainPage/Page.html");
+    res.redirect("/extrafindorasections/mainSection/mainSection.html");
 });
 
 // Rutas
